41952464dd5d26fe12cdc59f1df84b9c7edc2e8b
ahilon <ziela111@gmail.com>
2026-02-25 15:50:42 +0100

    Add sql alchemy database (#4)
    * add missing gitigniore file
    
    * add first schema model
    
    * add first database
    
    ---------
    
    Co-authored-by: PZielkowski <paw.zielkowski@gmail.com>

--- Forum/main.py
+++ Forum/main.py
@@ -1,19 +1,37 @@
-from fastapi import FastAPI
-from schemas import TravelPostResponse
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from database import Base, engine, SessionLocal
+import models
+import schemas
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-posts = []
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-def root():
-    return {"message": "Travel Forum Works!!"}
 
 @app.post("/posts")
-def create_post(post: TravelPostResponse) :
-    posts.append(post)
-    return {"message": "Dodano wpis!", "posts": posts}
+def create_post(post: schemas.TravelPostCreate, db: Session = Depends(get_db)):
+    db_post = models.TravelPost(
+        country=post.country,
+        total_cost=post.total_cost,
+        food=post.breakdown.food,
+        accommodation=post.breakdown.accommodation,
+        transport=post.breakdown.transport,
+        attractions=post.breakdown.attractions,
+    )
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
 
 @app.get("/posts")
-def list_posts():
-    return posts
+def list_posts(db: Session = Depends(get_db)):
+    return db.query(models.TravelPost).all()
--- Forum/schemas.py
+++ Forum/schemas.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
 
+
 class CostBreakdown(BaseModel):
     food: float
     accommodation: float
     transport: float
     attractions: float
     souvenirs: float
+
+class TravelPostCreate(BaseModel):
+    country: str
+    total_cost: float
+    breakdown: CostBreakdown
 
 class TravelPostResponse(BaseModel):
     country:str
